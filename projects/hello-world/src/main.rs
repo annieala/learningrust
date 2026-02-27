b73a2793e573ce02a2b337d13cb71d46144668d6
Annie Ala <anne@koat.ai>
2026-02-26 18:12:10 -0700

    Sync repo with local: remove .gitignore, add hello-world project, update structure

---
+++ projects/hello-world/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+// main always is the first code that runs in every executable rust program. 
+// a rust macro is way to write code that generates code to extend Rust syntax https://doc.rust-lang.org/book/ch01-02-hello-world.html
+// ! means your calling a macro not a normal function and macros don't always follow the same rules as functions. 
