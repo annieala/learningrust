5489c4cbc308a4616c468b1940f7e1b8d9dc643f
Annie Ala <anne@koat.ai>
2026-02-26 18:42:19 -0700

    just adding notes

---
+++ guessing-game/target/debug/deps/guessing_game-a332b968c3206e08.d
@@ -0,0 +1,5 @@
+/Users/annemarieala/Documents/learningrust/guessing-game/target/debug/deps/guessing_game-a332b968c3206e08.d: src/main.rs
+
+/Users/annemarieala/Documents/learningrust/guessing-game/target/debug/deps/guessing_game-a332b968c3206e08: src/main.rs
+
+src/main.rs:
---
+++ guessing-game/target/debug/guessing-game.d
@@ -0,0 +1,1 @@
+/Users/annemarieala/Documents/learningrust/guessing-game/target/debug/guessing-game: /Users/annemarieala/Documents/learningrust/guessing-game/src/main.rs
