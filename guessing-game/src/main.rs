652dec28d103a5b3a0dc9520a47bd07cda641dc2
Annie Ala <anne@koat.ai>
2026-02-26 20:09:24 -0700

    finally learning

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,6 +1,8 @@
-use std::io;  // "std" is a standard library. "io" is an input/output library in the standard library. 
+use std::io;
+use std::cmp::Ordering; // ordering type is another enum and has the variants Less, Greater and Equal. the cmp method compares two values and can be called on anything that can be compared. it takes reference to whatever you want to compare with.  
 
-use rand::Rng; 
+use rand::Rng;
+// Rng is a trait that defines methods that random number generators implement. we bring the Rng trait into scope to use the gen_range method on the random number generator we create in the next line.
 // main function is the entry point of the program 
 fn main() {  
     // println! is a macro that prints the string to the screen 
@@ -10,7 +12,8 @@ fn main() {
 
     println! ("The secret number is {secret_number}");
 
-    println!("Please input your guess."); 
+    loop { 
+        println!("Please input your guess."); 
     // mut for mutable variable, string::new() creates a new empty string, :: is a path seperator, operator to navigate name spaces 
     // new is an associated function of the String type, an associated function is a function implemented on a type in this case string 
     // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
@@ -26,5 +29,26 @@ fn main() {
     // 
     .expect("Failed to read line");   // expect is like an if else throw error pattern in one line. 
 
-    println!("You guessed {guess}")
+    let guess: u32 = match guess.trim().parse() { 
+        Ok(num) => num, // if the parse is successful, we get the number back and bind it to the variable guess 
+        Err(_) => continue, // if the parse fails, we get an error back and we ignore the error and start a new iteration of the loop
+    };
+
+    // let guess: u32 = guess.trim().parse().expect("Please type a number!"); // shadow the previous value of guess with a new one, lets us reuse the guess variable rather than forcing us to create two unique varaibles such as guess_str and guess. 
+    // trim eliminates any whitespace at the beginning and end of the string, including the newline character that is added when the user presses enter.
+    // parse converst a string to another type. 
+    // u32 unsigned 32 bit integer. 
+    println!("You guessed {guess}");
+
+    match guess.cmp(&secret_number) { // cmp is a method that compares the guess to the secret number and returns a variant of the Ordering enum we brough into scope with the use statement. the match expression decides what to do next based on which variant of ordering was returned from the call to cmp with the values in guess and secret_number
+        // a match expression is made up of arms an arm consists of a pattern to match against and the code that should be run if the value given to match fitms that arms pattern. rust takes the value given to match and looks through each arms parrtern in turn. 
+        Ordering::Less => println!("Too small!"), // if the guess is less than the secret number, print "Too small!"
+        Ordering::Greater => println!("Too big!"), // if the guess is greater than the secret number, print "Too big!"
+        Ordering::Equal => {
+            println!("You win!"); // if the guess is equal to the secret number, print "You win!"
+            break;
+        }
+    }
+    }
+
 }
